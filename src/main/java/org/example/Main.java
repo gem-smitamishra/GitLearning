eeb0b8ed423ed5d18b36de99ee8bfcc9aa91f72a
gem-smitamishra <smita.mishra@geminisolutions.com>
2023-03-13 11:17:25 +0530

    add

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+       FetchData();
+    }
+
+
+    public static void FetchData() throws IOException {
+        FileInputStream file = new FileInputStream(new File("C:/smita/DemoData.xlsx"));
+        XSSFWorkbook wb = new XSSFWorkbook(file);
+        XSSFSheet sheet = wb.getSheetAt(0);
+        Iterator<Row> itr= sheet.iterator();
+        while (itr.hasNext()){
+            Row row = itr.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()){
+                Cell cell = cellIterator.next();
+                System.out.println(cell.getCellType());
+               System.out.println(cell.getStringCellValue());
+            }
+        }
+
+    }
+}
