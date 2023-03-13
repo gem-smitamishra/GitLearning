615b7715125829b1e4a65f004c68046d254eb44f
gem-smitamishra <smita.mishra@geminisolutions.com>
2023-03-13 12:06:17 +0530

    Message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,6 +30,9 @@ public class Main {
                 System.out.println(cell.getCellType());
                System.out.println(cell.getStringCellValue());
 
+               System.out.println("Checking");
+
+
             }
         }
 
